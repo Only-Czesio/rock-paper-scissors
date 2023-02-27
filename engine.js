e1f5510924f738ce1207fc742c7770227f5cbba0
Only-Czesio <marek@kamyk.me>
2023-02-27 19:26:03 +0100

    It's working a little now
    You can play but it's not 100% ready

--- engine.js
+++ engine.js
@@ -1,9 +1,10 @@
 const marks = ["rock", "paper", "scissors"];
 
-const rock = document.querySelector("rock");
-const paper = document.querySelector("paper");
-const scissors = document.querySelector("scissors");
-const getRandom = document.querySelector("getRandom");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const getRandom = document.getElementById("getRandom");
+const botRandom = document.getElementById("botRandom");
 
 function randomChoice() {
 
@@ -15,23 +16,93 @@ rock.addEventListener("click", function () {
 
     const computeranswer = randomChoice();
 
+    // console.log(computeranswer);
+
     if (marks[computeranswer] === marks[0]) {
+
+        botRandom.textContent = marks[computeranswer];
 
         getRandom.textContent = "It's a tie!!";
 
     }
     if (marks[computeranswer] === marks[1]) {
 
-        getRandom.textContent = "You win!!";
+        botRandom.textContent = marks[computeranswer];
+
+        getRandom.textContent = "You loose!!";
 
     }
     if (marks[computeranswer] === marks[2]) {
+
+        botRandom.textContent = marks[computeranswer];
+
+        getRandom.textContent = "You win!!";
+
+    }
+
+});
+
+
+paper.addEventListener("click", function () {
+
+    const computeranswer = randomChoice();
+
+    console.log(computeranswer);
+
+    if (marks[computeranswer] === marks[0]) {
+
+        botRandom.textContent = marks[computeranswer];
+
+        getRandom.textContent = "You win!!";
+
+    }
+    if (marks[computeranswer] === marks[1]) {
+
+        botRandom.textContent = marks[computeranswer];
+
+        getRandom.textContent = "It's a tie!!";
+
+    }
+    if (marks[computeranswer] === marks[2]) {
+
+        botRandom.textContent = marks[computeranswer];
 
         getRandom.textContent = "You loose!!";
 
     }
 
-})
+});
+
+
+scissors.addEventListener("click", function () {
+
+    const computeranswer = randomChoice();
+
+    console.log(computeranswer);
+
+    if (marks[computeranswer] === marks[0]) {
+
+        botRandom.textContent = marks[computeranswer];
+
+        getRandom.textContent = "You loose!!";
+
+    }
+    if (marks[computeranswer] === marks[1]) {
+
+        botRandom.textContent = marks[computeranswer];
+
+        getRandom.textContent = "You win!!";
+
+    }
+    if (marks[computeranswer] === marks[2]) {
+
+        botRandom.textContent = marks[computeranswer];
+
+        getRandom.textContent = "It's a tie!!";
+
+    }
+
+});
 
 
 
